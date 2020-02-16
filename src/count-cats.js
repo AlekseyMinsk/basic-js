f28b406382890134a0056389277290b2702e3a77
AlekseiMinsk <budanov.info@gmail.com>
2020-02-16 23:58:45 +0300

    add count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+ 	var answer = 0;
+ 	matrix.forEach(item => {
+ 		item.forEach(i => {
+ 			if(i === "^^") {
+ 				answer++;
+ 			}
+ 		})
+ 	})
+ 	return answer;
 };
