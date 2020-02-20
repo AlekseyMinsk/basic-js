b18eeb81c4e13a965311cb721f9857194aa2c1f0
Aleksey Budanov <31888451+AlekseyMinsk@users.noreply.github.com>
2020-02-20 21:36:50 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,34 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+	if(!Array.isArray(arr)) {
+		throw Error();
+	} 
+
+  var answer = [];
+  for(var i = 0; i < arr.length; ++i) {
+		switch (arr[i]) {	 
+		  case "--discard-next":
+		    if(i < arr.length - 1) {
+		  		i++;
+		    }
+		    break;
+		  case "--discard-prev":
+		    if(answer.length) {
+		    	answer.pop();
+		    }
+		    break;
+		  case "--double-next":
+		    if(i < arr.length - 1) {
+		    	answer.push(arr[i + 1]);
+		    }
+		    break;
+		  case "--double-prev":
+		    if(i > 0) {
+		    	answer.push(arr[i - 1]);
+		    }
+		    break;
+		    default:
+	  			answer.push(arr[i]);
+		}
+  }
+  return answer;
 };
