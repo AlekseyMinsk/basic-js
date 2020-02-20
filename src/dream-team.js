318e3c2c518ca5866cda1af3330faef599abcaa9
Aleksey Budanov <31888451+AlekseyMinsk@users.noreply.github.com>
2020-02-20 11:47:52 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+ if(!Array.isArray(members)) {
+  return false;
+ }
+  var answer = [];
+  members.forEach(i => {
+   if(typeof i === 'string') {
+    answer.push(i.trim()[0].toUpperCase());
+   }   
+  })
+  return answer.sort().join("");
 };
