09a836a43383db7ac923ead5d9fd0430960381f9
Aleksey Budanov <31888451+AlekseyMinsk@users.noreply.github.com>
2020-02-27 14:55:57 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,29 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+
+    str = "" + str;  
+    var separator = options.separator ? options.separator : "+";
+    var additionSeparator = options.additionSeparator ? options.additionSeparator : "+";
+
+
+    if(options.addition !== undefined) {
+		var addition = "" + options.addition;
+    	if(+options.additionRepeatTimes) {
+    		for(var i = 1; i < options.additionRepeatTimes; i++ ) {
+    			addition = addition + additionSeparator + options.addition;
+    		}
+    		str = str + addition;
+    	} else {
+    		str = str + addition;
+    	}
+    }
+    var answer = str;
+    
+    if(+options.repeatTimes) {
+    	for(var i = 1; i < options.repeatTimes; i++ ) {
+    		answer = answer + separator + str;
+    	}
+    }
+
+    return answer;
 };
   
